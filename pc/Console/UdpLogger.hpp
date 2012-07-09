217b3357f77120142add823a7084fb48edad0e4d
dsk7 <dsk7>
2012-07-09 22:51:47 +0200

    console: added header to csv log

--- pc/Console/UdpLogger.hpp
+++ pc/Console/UdpLogger.hpp
@@ -17,6 +17,9 @@ public:
   UdpLogger(boost::asio::io_service& ios, uint16_t port, const string& logfile) : socket_(ios, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port))
   {
     log_.open(logfile.c_str(), std::ios::app);
+    log_ << "seq,time,ax,ay,az,gx,gy,gz,h_vel,set_h,height,throttle,set_pitch,att_pitch,adj_pitch,set_roll,att_roll,adj_roll,set_yaw,att_yaw,adj_yaw" << std::endl;
+    
+    
   }
   void start()
   {
