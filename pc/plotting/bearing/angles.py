e1c971bfe2391c5d2f11cac36f437e8da8eda26a
DSK7 <some@one.com>
2012-07-29 22:20:11 +0200

    bearing-plots: refactored csv reader

--- pc/plotting/bearing/angles.py
+++ pc/plotting/bearing/angles.py
@@ -3,25 +3,25 @@ from numpy import *
 from pylab import *
 from testlib import *
 
-(ax,ay,az,gx,gy,gz,mx,my,mz)=read_droneAngs('logs/drone_1_1.csv')
+d=readCsv('logs/drone_1_1.csv')
 
 a=subplot(3,1,1)
-a.plot(ax, label="ax")
-a.plot(ay, label="ay")
-a.plot(az, label="az")
+a.plot(d.ax, label="ax")
+a.plot(d.ay, label="ay")
+a.plot(d.az, label="az")
 legend()
 
 g=subplot(3,1,2)
-g.plot(gx, label="gx")
-g.plot(gy, label="gy")
-g.plot(gz, label="gz")
+g.plot(d.gx, label="gx")
+g.plot(d.gy, label="gy")
+g.plot(d.gz, label="gz")
 legend()
 
 
 m=subplot(3,1,3)
-m.plot(mx, label="mx")
-m.plot(my, label="my")
-m.plot(mz, label="mz")
+m.plot(d.mx, label="mx")
+m.plot(d.my, label="my")
+m.plot(d.mz, label="mz")
 legend()
 
 show()
--- pc/plotting/bearing/vecsum.py
+++ pc/plotting/bearing/vecsum.py
@@ -4,10 +4,10 @@ from pylab import *
 from testlib import *
 from math import *
 
-(ax,ay,az,gx,gy,gz,mx,my,mz)=read_droneAngs('logs/drone1_1.csv')
+d=readCsv('logs/drone_1_1.csv')
 
-ax=[ a *-1 for a in ax]
-az=[ a *-1 for a in az]
+ax=[ a *-1 for a in d.ax]
+az=[ a *-1 for a in d.az]
 
 
 ls=[sqrt(x*x+z*z)/9.81 for (x,z) in zip(ax,az)]
